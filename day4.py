323ca3c6552b83923bc105dd1d28c0b2bf9ec5fc
Navneet parashar <navneetparashar12345@gmail.com>
2026-07-16 20:44:30 +0530

    Refactor findMedianSortedArrays to use merged array

--- day4.py
+++ day4.py
@@ -1,28 +1,27 @@
 class Solution:
     def findMedianSortedArrays(self, nums1, nums2):
-        if len(nums1) > len(nums2):
-            nums1, nums2 = nums2, nums1
+        arr = []
+        i = j = 0
 
-        m, n = len(nums1), len(nums2)
-        left, right = 0, m
-
-        while left <= right:
-            i = (left + right) // 2
-            j = (m + n + 1) // 2 - i
-
-            leftA = float("-inf") if i == 0 else nums1[i - 1]
-            rightA = float("inf") if i == m else nums1[i]
-
-            leftB = float("-inf") if j == 0 else nums2[j - 1]
-            rightB = float("inf") if j == n else nums2[j]
-
-            if leftA <= rightB and leftB <= rightA:
-                if (m + n) % 2 == 0:
-                    return (max(leftA, leftB) + min(rightA, rightB)) / 2.0
-                else:
-                    return max(leftA, leftB)
-
-            elif leftA > rightB:
-                right = i - 1
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] <= nums2[j]:
+                arr.append(nums1[i])
+                i += 1
             else:
-                left = i + 1
+                arr.append(nums2[j])
+                j += 1
+
+        while i < len(nums1):
+            arr.append(nums1[i])
+            i += 1
+
+        while j < len(nums2):
+            arr.append(nums2[j])
+            j += 1
+
+        n = len(arr)
+
+        if n % 2 == 1:
+            return arr[n // 2]
+        else:
+            return (arr[n // 2 - 1] + arr[n // 2]) / 2.0
